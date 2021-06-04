04f71321650f41502af40f90c71a26c85398c849
1HenriqueMata1 <57409783+1HenriqueMata1@users.noreply.github.com>
2021-06-04 22:53:13 +0100

    project conclued

---
+++ weather-vue-app/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <Weather/>
+  </div>
+</template>
+
+<script>
+import Weather from './components/Weather.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Weather
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'montserrat', sans-serif;
+}
+
+#app {
+  background-image: url('./assets/cold-bg.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+#app.warm {
+  background-image: url('./assets/warm-bg.jpg');
+}
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+}
+</style>
